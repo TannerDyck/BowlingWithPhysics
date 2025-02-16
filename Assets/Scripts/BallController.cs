63fa082bbe161f635faf52d8423a3f4194041e36
TannerDyck <97553206+TannerDyck@users.noreply.github.com>
2025-02-15 20:56:33 -0800

    Completed Section 4.3 (Reset)
    All of the reset logic seems to be working. Fixed that one pin falling through the ground again, and dramatically increased the FallTrigger width for detecting if pins have fallen down. Pins were frequently laying on top of others so that their hitbox was upwards on an angle and not making contact with the ground. So I widened it so that if a pin is more than roughly 45 degrees towards the ground the isPinFallen boolean should trigger, giving more accurate scores.

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -15,10 +15,13 @@ public class BallController : MonoBehaviour
     void Start()
     {
         ballRB = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
         inputManager.OnSpacePressed.AddListener(LaunchBall);
-        transform.parent = ballAnchor;
+        ResetBall();
+
+        /*transform.parent = ballAnchor;
         transform.localPosition = Vector3.zero;
-        ballRB.isKinematic = true;
+        ballRB.isKinematic = true;*/
     }
 
     private void LaunchBall()
@@ -32,5 +35,15 @@ public class BallController : MonoBehaviour
         ballRB.isKinematic = false;
         ballRB.AddForce(launchIndicator.forward * force, ForceMode.Impulse);
         launchIndicator.gameObject.SetActive(false);
+    }
+
+    public void ResetBall()
+    {
+        isBallLaunched = false;
+        //We are setting the ball to be a Kinematic Body
+        ballRB.isKinematic = true;
+        launchIndicator.gameObject.SetActive(true);
+        transform.parent = ballAnchor;
+        transform.localPosition = Vector3.zero;
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,18 @@
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private float score = 0;
+    [SerializeField] private BallController ball;
+    [SerializeField] private GameObject pinCollection;
+    [SerializeField] private Transform pinAnchor;
+    [SerializeField] private InputManager inputManager;
     [SerializeField] private TextMeshProUGUI scoreText;
     private FallTrigger[] pins;
+    private FallTrigger[] fallTriggers;
+    private GameObject pinObjects;
 
     private void Start()
     {
@@ -18,11 +25,45 @@ public class GameManager : MonoBehaviour
         {
             pin.OnPinFall.AddListener(IncrementScore);
         }
+
+        inputManager.OnResetPressed.AddListener(HandleReset);
+        SetPins();
     }
 
     private void IncrementScore()
     {
         score++;
         scoreText.text = $"Score: {score}";
+    }
+
+    private void HandleReset()
+    {
+        ball.ResetBall();
+        SetPins();
+    }
+
+    private void SetPins()
+    {
+        // We first make sure that all the previous pins have been destroyed
+        // this is so that we don't create a new collection of
+        //standing pins on top of already fallen pins
+        if(pinObjects)
+        {
+            foreach (Transform child in pinObjects.transform)
+            {
+                Destroy(child.gameObject);
+            }
+            Destroy(pinObjects);
+        }
+        // We then instatiate a new set of pins to our pin anchor transform
+        pinObjects = Instantiate(pinCollection, pinAnchor.transform.position, Quaternion.identity, transform);
+        // We add the Increment Score function as a listener to
+        // the OnPinFall event each of new pins
+        fallTriggers = FindObjectsByType<FallTrigger>(FindObjectsInactive.Include,
+        FindObjectsSortMode.None);
+        foreach (FallTrigger pin in fallTriggers)
+        {
+            pin.OnPinFall.AddListener(IncrementScore);
+        }
     }
 }

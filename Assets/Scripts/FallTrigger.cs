ea73906395a5211f7aaf1ec5bf18bda49fbcd598
TannerDyck <97553206+TannerDyck@users.noreply.github.com>
2025-02-15 20:14:53 -0800

    Completed Section 4.1 (Fall Trigger / Tags)
    Added Fall Trigger logic for the pins, triggers the debug log 10 times for a strike and accurately counts how many pins were knocked down in all other outcomes. I also tried to add the Trigger / Tag logic for the gutters, so that the gutters wouldn't shoot pins out, not sure if it was correct but I tried!

---
+++ Assets/Scripts/FallTrigger.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class FallTrigger : MonoBehaviour
+{
+    //Adding an OnPinFall public event in case any other object
+    //needs to know whether a Pin has Fallen i.e. Our GameManager
+
+    public UnityEvent OnPinFall = new();
+    public bool isPinFallen = false;
+    
+    private void OnTriggerEnter(Collider triggeredObject)
+    {
+        // We can use the CompareTag() function
+        // to compare the collided object's tag to a string
+        // We also make sure that the OnPinFall is not invoked more than once
+        // For example if a pin were to bounce off the ground and hit it twice
+        if (triggeredObject.CompareTag("Ground") && !isPinFallen)
+        {
+            isPinFallen = true;
+            OnPinFall?.Invoke();
+            Debug.Log($"{gameObject.name} is fallen");
+        }
+    }
+}
